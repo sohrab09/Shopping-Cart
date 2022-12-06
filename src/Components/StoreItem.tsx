42cc654d3146529ad15609cf01498ab0a193191e
sohrab09 <sohrab@nassagroup.org>
2022-12-06 11:30:05 +0600

    Card design --> Add, Remove button added

--- src/Components/StoreItem.tsx
+++ src/Components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { formatCurrency } from "../Utilities/FormatCurrency";
 
 type StoreItemProps = {
@@ -9,8 +9,10 @@ type StoreItemProps = {
 };
 
 export function StoreItems({ id, name, price, img }: StoreItemProps) {
+  const quantity = 0;
+
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={img}
@@ -21,6 +23,29 @@ export function StoreItems({ id, name, price, img }: StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div className="d-flex align-items-center justify-content-center">
+                <Button variant="outline-secondary" className="rounded-0">
+                  -
+                </Button>
+                <span className="px-3 fs-3">{quantity}</span>
+                <Button variant="outline-secondary" className="rounded-0">
+                  +
+                </Button>
+              </div>
+              <Button variant="danger" className="w-100">
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
