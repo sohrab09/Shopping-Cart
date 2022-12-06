967b1d7fd5f07b83c0e4c420c88dd33de35162a8
sohrab09 <sohrab@nassagroup.org>
2022-12-06 10:30:05 +0600

    Navbar Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Store from "./Pages/Store";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
